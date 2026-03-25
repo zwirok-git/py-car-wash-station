af91b6c57415a9a48c87032bdc9d96965b4ea436
Oleksii Prokopenko <oleksijprokopenko@gmail.com>
2026-03-25 14:12:17 +0100

    Update main.py

--- app/main.py
+++ app/main.py
@@ -12,12 +12,12 @@ class Car:
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: int,
+    def __init__(self, distance_from_city_center: float,
                  clean_power: int,
                  average_rating: float,
                  count_of_ratings: int) -> None:
 
-        self.distance_from_city_center = distance_from_city_center
+        self.distance_from_city_center = round(distance_from_city_center)
         self.clean_power = clean_power
         self.average_rating = round(average_rating, 1)
         self.count_of_ratings = count_of_ratings
